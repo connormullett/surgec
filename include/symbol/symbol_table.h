ca63b3143fecbdb3729bf082a5d336fb63bdb438
connor <connormullett@gmail.com>
2020-05-31 03:54:38 -0400

    symbol table implementation

--- include/symbol/symbol_table.h
+++ include/symbol/symbol_table.h
@@ -4,6 +4,8 @@
 #include <string>
 #include <unordered_map>
 
+#include "parser/nodes.h"
+
 using std::pair;
 using std::string;
 using std::unordered_map;
@@ -14,24 +16,45 @@ enum SymbolType {
 };
 
 class Symbol {
-private:
+protected:
     SymbolType _type;
+    string identifier;
 
 public: 
-    Symbol(SymbolType type);
+    Symbol(string ident);
     SymbolType get_type();
-
+    string get_identifier();
 };
 
+class IntegerSymbol : public Symbol {
+private:
+    int _value;
+
+public:
+    IntegerSymbol(string identifier, int value);
+    int get_value();
+};
+
+class FunctionSymbol : public Symbol {
+private:
+    FunctionSymbol(string identifier, Node* body);
+};
+
+/*
+todo : per scope symbol table and 
+todo : chaining with procedures (functions)
+*/
 
 static class SymbolTable {
 private:
-    unordered_map<string, Symbol> symbol_table;
+    unordered_map<string, Symbol*> symbol_table;
 
 public:
-    void push_symbol(string key, Symbol symbol);
+    void push_symbol(string key, Symbol* symbol);
     bool key_exists(string key);
+    void drop_symbol(string key);
+    Symbol* get_symbol(string key);
 
-    Symbol get_symbol(string key);
+    ~SymbolTable();
 
 } symbol_table;
--- src/symbol/symbol_table.cpp
+++ src/symbol/symbol_table.cpp
@@ -7,23 +7,54 @@ using std::string;
 using std::unordered_map;
 
 
-Symbol::Symbol(SymbolType type) {
-    _type = type;
+Symbol::Symbol(string ident) {
+    identifier = ident;
 }
 
 SymbolType Symbol::get_type() {
     return _type;
 }
 
+string Symbol::get_identifier() {
+    return identifier;
+}
 
-void SymbolTable::push_symbol(string key, Symbol symbol) {
 
+IntegerSymbol::IntegerSymbol(string identifier, int value) :
+    Symbol(identifier)
+{
+    _type = SymbolType::IntVariable;
+}
+
+int IntegerSymbol::get_value() {
+    return _value;
+}
+
+
+FunctionSymbol::FunctionSymbol(string identifier, Node* body) :
+    Symbol(identifier)
+{
+    _type = SymbolType::FunctionDeclaration;
+}
+
+
+void SymbolTable::push_symbol(string key, Symbol* symbol) {
+    symbol_table.insert( std::make_pair(key, symbol) );
 }
 
 bool SymbolTable::key_exists(string key) {
-
+    if (symbol_table.find(key) != symbol_table.end()) return true;
+    else return false;
 }
 
-Symbol SymbolTable::get_symbol(string key) {
-    
+Symbol* SymbolTable::get_symbol(string key) {
+    return symbol_table.at(key);
+}
+
+void SymbolTable::drop_symbol(string key) {
+    symbol_table.erase(key);
+}
+
+SymbolTable::~SymbolTable() {
+    symbol_table.clear();
 }
